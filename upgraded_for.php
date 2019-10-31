1946fceba2c9f013a4d4454454ef38e144c995a7
Maro Balestracci <2109711@talnet.nl>
2019-10-31 10:30:29 +0100

    Uitwerking van de exercise

---
+++ upgraded_for.php
@@ -0,0 +1,17 @@
+<?php
+	$namen=array(
+	"Jeff",
+	"Joost",
+	"Jerry",
+	"Joe"
+);
+	$number=array(
+	 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+);
+	foreach ($namen as $value) {
+		echo "$value" . PHP_EOL;
+}
+	foreach ($number as $count20) {
+		echo "$count20" . PHP_EOL;
+}
+	exit();
